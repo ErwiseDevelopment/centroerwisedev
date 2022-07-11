b692fd978eda54e4e2721afd872ec1d2ceea48cb
ErwiseDevelopment <erwisedev@gmail.com>
2022-07-11 08:49:38 -0300

    banner

--- wp-content/themes/wp-bootstrap-starter-child/template-parts/content-banner.php
+++ wp-content/themes/wp-bootstrap-starter-child/template-parts/content-banner.php
@@ -1,2 +1,11 @@
-<section style="width:100%;height:100vh;background:red">
+<section>
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-12 px-0">
+                <?php echo do_shortcode( '[rev_slider alias="slider-1"][/rev_slider]' ); ?> 
+            </div>
+        </div>
+    </div>
 </section>
